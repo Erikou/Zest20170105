491489beb53abeccd13ad31e18fc263dba094e1b
Louis Aumaitre <darksteelpenguin@gmail.com>
2016-09-13 19:07:14 +0800

    Add missing file

---
+++ src/SE/PerformanceBundle/Resources/public/js/daily/daily.js
@@ -0,0 +1,270 @@
+$(document).ready(function() {
+
+  Highcharts.createElement('link',{href:'//fonts.googleapis.com/css?family=Dosis:400,600',rel:'stylesheet',type:'text/css'},null,document.getElementsByTagName('head')[0]);Highcharts.theme={colors:["#7cb5ec","#f7a35c","#90ee7e","#7798BF","#aaeeee","#ff0066","#eeaaee","#55BF3B","#DF5353","#7798BF","#aaeeee"],chart:{backgroundColor:null,style:{fontFamily:"Dosis, sans-serif"}},title:{style:{fontSize:'16px',fontWeight:'bold',textTransform:'uppercase'}},tooltip:{borderWidth:0,backgroundColor:'rgba(219,219,216,0.8)',shadow:false},legend:{itemStyle:{fontWeight:'bold',fontSize:'13px'}},xAxis:{gridLineWidth:1,labels:{style:{fontSize:'12px'}}},yAxis:{minorTickInterval:'auto',title:{style:{textTransform:'uppercase'}},labels:{style:{fontSize:'12px'}}},plotOptions:{candlestick:{lineColor:'#404048'}},background2:'#F0F0EA'};Highcharts.setOptions(Highcharts.theme);
+
+  var dailyJson;
+  var dateVal = $('#dailyDate').val();
+  var inputTable;
+
+  $.get(
+    ajaxDaily,               
+    {date: dateVal}, 
+    function(response){
+      createGauge(response.dailyJson, 0, 0);
+      replaceTotalData(response.dailyJson, 0,0);
+      dailyJson = response.dailyJson;
+      $('.panel-default .dataTables_scrollHeadInner').css('padding-left','0px');
+      $('.panel-default .table').css('margin-bottom','0px');
+      $("*[data-toggle='tooltip']").tooltip();
+      inputTable = $('#inputs').DataTable( {
+        retrieve: true,
+        "scrollY": "200px",
+        paging: false,
+        "dom": 'lrtip',
+        "info": false
+      });
+      inputTable.rows.add(response.template).draw();
+    },
+    "json");
+
+  $('#filters a').click(function(){
+    $this = $(this);
+    filterData($this, dailyJson);
+  });
+
+  $(document).on('change', 'form input#dailyDate', function(e){
+    dateVal = $('#dailyDate').val();
+    $.get(
+    ajaxDaily,               
+    {date: dateVal}, 
+    function(response){
+      createGauge(response.dailyJson, 0, 0);
+      replaceTotalData(response.dailyJson, 0,0);
+      $('#filters .teams').html(response.filters);//update filters
+      dailyJson = response.dailyJson;
+      inputTable = $('#inputs').DataTable();
+      inputTable.clear();
+      inputTable.rows.add(response.template).draw();
+      inputTable.draw();
+      $('#filters a').click(function(){
+        $this = $(this);
+        filterData($this, dailyJson);
+      });
+    },
+    "json");
+  });
+
+});
+
+function createGauge(json, team, shift){
+  var gaugeOptions = {
+
+    chart: {
+        type: 'solidgauge'
+    },
+
+    title: {
+            text: 'Productivity'
+        },
+
+    pane: {
+        center: ['50%', '50%'],
+        size: '100%',
+        startAngle: 0,
+        endAngle: 360,
+        background: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#DF5353'], // red
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#55BF3B'] // green
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickPixelInterval: 400,
+        tickWidth: 0,
+        title: {
+            y: 140
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+  };
+  // The speed gauge
+$('#container-prod').highcharts(Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: 0,
+        max: 10,
+        title: {
+            text: ''
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Productivity',
+        data: [json[team][shift]['report']['prod']],
+        dataLabels: {
+            format: '<div style="text-align:center; margin-top:-70px;"><span style="font-size:90px;color:' +
+                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                   '<span style="font-size:12px;color:black">to line/h</span></div>'
+        },
+        tooltip: {
+            valueSuffix: 'to line/h'
+        }
+    }]
+}));
+
+$('#container-activities').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Activities Usage & Efficiency'
+        },
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            categories: json[team][shift]['activities']['cat']
+        },
+        yAxis: [{
+            min: 0,
+            title: {
+                text: 'Hours'
+            }
+        }, {
+            min: 0,
+            title: {
+                text: 'Ke (%)'
+            },
+            opposite: true
+        }],
+        legend: {
+            shadow: false
+        },
+        tooltip: {
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                grouping: false,
+                borderWidth: 0,
+                shadow: false
+            }
+        },
+        series: [{
+            name: 'Activities Manhours',
+            color: 'rgba(165,170,217,1)',
+            data: json[team][shift]['activities']['data'],
+            tooltip: {
+                valueSuffix: ' h'
+            },
+            pointPadding: 0.3,
+            pointPlacement: -0.2
+        }, {
+            name: 'Ke',
+            color: 'rgba(248,161,63,1)',
+            data: json[team][shift]['activities']['ke'],
+            tooltip: {
+                valueSuffix: ' %'
+            },
+            pointPadding: 0.3,
+            pointPlacement: 0.2,
+            yAxis: 1
+        }]
+    });
+}
+
+function replaceTotalData(j, t, s){
+  $('#report-panel #to').html(j[t][s]['report']['to']);
+  $('#report-panel #ma').html(j[t][s]['report']['mh']);
+  $('#report-panel #wh').html(j[t][s]['report']['wh']);
+  $('#report-panel #hc').html(j[t][s]['report']['hc']);
+  $('#report-panel #ot').html(j[t][s]['report']['ot']);
+  $('#report-panel #mto').html(j[t][s]['report']['mto']);
+  $('#report-panel #tr').html(j[t][s]['report']['tr']);
+  $('#report-panel #ab').html(j[t][s]['report']['ab']);
+  $('#report-panel #le').html(j[t][s]['report']['le']);
+  $('#report-panel #ksr').html(j[t][s]['report']['ksr']);
+}
+
+function filterData($this, json){
+  $this.siblings().removeClass('label-primary').addClass('label-default');
+  $this.removeClass('label-default').addClass('label-primary');
+    
+  //update charts data
+  var containerProd = $('#container-prod').highcharts();
+  var containerAct = $('#container-activities').highcharts();
+
+  if ($this.parent().attr('id') == 1){//team
+    if($this.attr('data-max-shift') == 1){
+      if(!$('#filters .shifts').hasClass('hide')){
+        $('#filters .shifts').addClass('hide');
+      }
+    }else if ($this.attr('data-max-shift') == 2){
+     $('#filters .shifts').removeClass('hide');
+     if(!$('#filters .shifts #3').hasClass('hide')){
+        $('#filters .shifts #3').addClass('hide');
+      } 
+    }else{
+      $('#filters .shifts').removeClass('hide');
+      $('#filters .shifts #3').removeClass('hide');
+    }
+    $('#filters .shifts a').removeClass('label-primary').addClass('label-default');
+    $('#filters .shifts #0').removeClass('label-default').addClass('label-primary');
+
+    loadCharts(json, $this.attr('id'), 0, containerProd, containerAct);
+
+    if($this.attr('id') != 0){
+      $('#inputs').DataTable().column(0).search($this.text()).draw();  
+    }else{
+      $('#inputs').DataTable().column(0).search("").draw();  
+    }
+    //if filter by team, reset filter shift to all
+    $('#inputs').DataTable().column(1).search("").draw(); 
+  }else{
+   //shift
+   var teamId = $('#filters #1').find('.label-primary').attr('id');
+   loadCharts(json, teamId, $this.attr('id'), containerProd, containerAct);
+   if($this.attr('id') != 0){
+      $('#inputs').DataTable().column(1).search($this.attr('id')).draw();  
+    }else{
+      $('#inputs').DataTable().column(1).search("").draw();  
+    }
+  }
+}
+
+function loadCharts(j, t, s, p, a){
+  p.series[0].setData([j[t][s]['report']['prod']]);
+  a.xAxis[0].setCategories(j[t][s]['activities']['cat']);
+  a.series[0].setData(j[t][s]['activities']['data']);
+  a.series[1].setData(j[t][s]['activities']['ke']);
+  replaceTotalData(j,t,s);
+}
---
+++ src/SE/PerformanceBundle/Resources/public/js/hr/hr.js
@@ -0,0 +1,377 @@
+$(document).ready(function() {
+
+Highcharts.createElement('link',{href:'//fonts.googleapis.com/css?family=Dosis:400,600',rel:'stylesheet',type:'text/css'},null,document.getElementsByTagName('head')[0]);Highcharts.theme={colors:["#7cb5ec","#f7a35c","#90ee7e","#7798BF","#aaeeee","#ff0066","#eeaaee","#55BF3B","#DF5353","#7798BF","#aaeeee"],chart:{backgroundColor:null,style:{fontFamily:"Dosis, sans-serif"}},title:{style:{fontSize:'16px',fontWeight:'bold',textTransform:'uppercase'}},tooltip:{borderWidth:0,backgroundColor:'rgba(219,219,216,0.8)',shadow:false},legend:{itemStyle:{fontWeight:'bold',fontSize:'13px'}},xAxis:{gridLineWidth:1,labels:{style:{fontSize:'12px'}}},yAxis:{minorTickInterval:'auto',title:{style:{textTransform:'uppercase'}},labels:{style:{fontSize:'12px'}}},plotOptions:{candlestick:{lineColor:'#404048'}},background2:'#F0F0EA'};Highcharts.setOptions(Highcharts.theme);
+
+var attJson;
+var monthVal;
+var yearVal;
+var attendanceTable;
+initData();
+
+monthVal = parseInt($('.monthpick').val()) + 1;
+yearVal = $('.yearpick').val();
+$('form select').addClass('form-control');
+
+$.get(
+    ajaxAttendance,               
+    {
+        month: monthVal,
+        year: yearVal
+    }, 
+    function(r){
+        attJson = r.template;
+        createTable(attJson, r.headers, r.daysNb);
+        createCharts(r.jsonData, 0, 0, r.headers, r.daysNb);
+        $('#filters a').click(function(){
+          $this = $(this);
+          filterData($this, attJson, r.jsonData);
+        });
+    },
+    "json"); 
+
+$('.close').click(function(){$('.table-panel').toggleClass('col-md-12 col-md-9').siblings('.entry-panel').addClass('hide');});
+
+$(document).on('change', '.month select', function(e){
+  monthVal = parseInt($('.monthpick').val()) + 1;
+  yearVal = $('.yearpick').val();
+  $.get(
+    ajaxAttendance,               
+    {
+      month: monthVal,
+      year: yearVal
+    }, 
+    function(r){
+      $('#filters .teams').html(r.filters);//update filters
+      attJson = r.template; 
+      $('#filters a').click(function(){
+        $this = $(this);
+        filterData($this, attJson, r.jsonData);
+      });
+        var attendanceTable = $('#attendance').DataTable();
+        attendanceTable.clear();
+        attendanceTable.destroy();
+        $('#attendance thead .day-h').remove();
+        createTable(attJson, r.headers, r.daysNb);
+        createCharts(r.jsonData, 0, 0, r.headers, r.daysNb);
+    },
+    "json");
+});
+
+});
+
+function initData(){
+    var d = new Date();
+    $('#monthpicker').monthpicker({'minYear' : 2015, 'maxYear' : 2016});
+    $('.monthpick').val(d.getMonth());
+    $('.yearpick').val(d.getFullYear());
+}
+
+function createCharts(json, t, s, h, n){
+
+  $('#summary-panel #mar').html(json[t][s]['report']['attrate']);
+  $('#summary-panel #hr').html(json[t][s]['report']['totalhr']);
+  $('#summary-panel #ot').html(json[t][s]['report']['totalothr']);
+  $('#summary-panel #otn').html(json[t][s]['report']['wdot']);
+  $('#summary-panel #phot').html(json[t][s]['report']['weot']);
+
+  $('#summary-panel #high').html($('#inject-employee .orange').length);
+  $('#summary-panel #low').html($('#inject-employee .yellow').length);
+  $('#summary-panel #miss').html($('#inject-employee .marron').length);
+  $('#summary-panel #leaves').html($('#inject-employee .blue').length);
+
+  var categories = [];
+  for (var i=1; i <= n; i++) {
+        if(typeof(h[i]) != "undefined" && h[i] !== null) {categories.push(h[i][0]['id']);}
+    };
+      
+  $('#container-attendance').highcharts({
+    chart: {
+            type: 'area'
+        },
+    title: {
+        text: 'Monthly attendance rate & Overtime Usage',
+        x: -20 //center
+    },
+    xAxis: {
+        categories: categories,
+        title: {
+                text: null
+            }
+    },
+    yAxis: [{
+        title: {
+            text: 'Attendance Rate %'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }],
+        min: 0,
+        max:100
+    },{
+        min: 0,
+        title: {
+            text: 'Overtime Hours'
+        },
+        opposite: true
+    }],
+    credits: {
+        enabled: false
+    },
+    tooltip: {
+      shared: false,
+      formatter: function() {
+        var text = '';
+        if(this.series.name.match("^Overtime")) {
+            text = '<span style="color:' + this.series.color + '">\u25CF</span><b> ' + this.x + ' - ' + this.series.name + '<br> Overtime: <b>' + Highcharts.numberFormat(this.y, 0) + 'h </b><br>  Headcount: <b>' + this.point.tip[1] + '</b><br> Overtime rate: <b>' + this.point.tip[2] + '%</b>';
+        } else {
+            text = this.x + ' <br>' + '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(this.y, 0) + '%</b>';
+        }
+        return text;
+      }
+    },
+    legend: {
+        layout: 'horizontal',
+        align: 'right',
+        verticalAlign: 'center',
+        borderWidth: 0
+    },
+    series: [{
+        name: 'Attendance - ' + $('#filters .teams').find('.label-primary').text(),
+        data: json[0][0]['attrate']['data'],
+    },{
+        name: 'Overtime - ' + $('#filters .teams').find('.label-primary').text(),
+        data: json[0][0]['otconso']['data'],
+        yAxis: 1    
+    }]
+  });
+
+  $('#container-daily-ot').highcharts({
+    chart: {
+            type: 'column'
+        },
+    title: {
+        text: 'Overtime over the week',
+        x: -20 //center
+    },
+    xAxis: {
+        categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        title: {
+                text: null
+            }
+    },
+    yAxis: {
+        title: {
+            text: ''
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
+    },
+    credits: {
+        enabled: false
+    },
+    tooltip: {
+        valueSuffix: 'h'
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'center',
+        borderWidth: 0
+    },
+    series: [{
+        name: $('#filters .teams').find('.label-primary').text(),
+        data: json[0][0]['dailyot']['data']
+    }]
+  });
+}
+
+function filterData($this, json, data){
+  $this.siblings().removeClass('label-primary').addClass('label-default');
+  $this.removeClass('label-default').addClass('label-primary');
+
+  //update charts data
+  var containerAttendance = $('#container-attendance').highcharts();
+  var containerDailyOt = $('#container-daily-ot').highcharts();
+    
+  if ($this.parent().attr('id') == 1){//team
+    if($this.attr('data-max-shift') == 1){
+      if(!$('#filters .shifts').hasClass('hide')){
+        $('#filters .shifts').addClass('hide');
+      }
+    }else if ($this.attr('data-max-shift') == 2){
+     $('#filters .shifts').removeClass('hide');
+     if(!$('#filters .shifts #3').hasClass('hide')){
+        $('#filters .shifts #3').addClass('hide');
+      } 
+    }else{
+      $('#filters .shifts').removeClass('hide');
+      $('#filters .shifts #3').removeClass('hide');
+    }
+    $('#filters .shifts a').removeClass('label-primary').addClass('label-default');
+    $('#filters .shifts #0').removeClass('label-default').addClass('label-primary');
+
+    if($this.attr('id') != 0){
+      $('#attendance').DataTable().column(1).search($this.text()).draw();  
+    }else{
+      $('#attendance').DataTable().column(1).search("").draw();  
+    }
+    //if filter by team, reset filter shift to all
+    $('#attendance').DataTable().column(2).search("").draw(); 
+
+    chartsFilter(data, $this.attr('id'), 0, containerAttendance, containerDailyOt);
+  }else{
+   //shift
+   if($this.attr('id') != 0){
+      $('#attendance').DataTable().column(2).search($this.attr('id')).draw();  
+    }else{
+      $('#attendance').DataTable().column(2).search("").draw();  
+    }
+    chartsFilter(data, $('#filters #1').find('.label-primary').attr('id'), $this.attr('id'), containerAttendance, containerDailyOt);
+  }
+}
+
+function createTable(j, h, n){
+
+    for (var i=1; i <= n; i++) {
+        if(typeof(h[i]) != "undefined" && h[i] !== null) {
+            $("<th id='"+i+"' class='day-h'>"+h[i][0]['id']+"</th>").insertBefore('#total');
+            if( h[i][0]['hd'] ){$('#attendance thead #'+i).addClass('holiday');}
+            else if( !h[i][0]['wd'] ){$('#attendance thead #'+i).addClass('weekend');}
+        }
+    };
+
+    attendanceTable = $('#attendance').DataTable( {
+        scrollY:        "480px",
+        scrollX:        true,
+        scrollCollapse: true,
+        paging:         false,
+        fixedColumns:   true,
+        retrieve: true,
+        "dom": 'lfrtip',
+        "info": false,
+        "columnDefs": [
+            {
+                "targets": [ 1 ],
+                "visible": false,
+            },
+            {
+                "targets": [ 2 ],
+                "visible": false
+            }
+        ]
+    });
+    attendanceTable.rows.add(j).draw();
+
+    $("*[data-toggle='tooltip']").tooltip({html :'true',container: 'body'});
+    $('.e-day-ok').closest('td').addClass('green box');
+    $('.e-day-low').closest('td').addClass('yellow box');
+    $('.e-day-high').closest('td').addClass('orange box');
+    $('.e-day-absent').closest('td').addClass('grey box');
+    $('.e-day-leave').closest('td').addClass('blue box');
+    $('.e-day-half').closest('td').addClass('bluegreen box');
+    $('.e-day-missing').closest('td').addClass('marron text-center');
+    $('#attendance .box').not('.grey').append("<div class='overlay'><i class='glyphicon glyphicon-info-sign'></i></div>");
+
+    $('#attendance .box').not('.grey').click(function(){
+          $this = $(this);
+          if($('.table-panel').hasClass('col-md-12')){
+              $('.table-panel').toggleClass('col-md-12 col-md-9').siblings('.entry-panel').removeClass('hide');
+          }
+          displayDetails($this);
+    });
+
+    $('#attendance_filter').prependTo('.table-panel .panel-heading').addClass('pull-right').html($('#attendance_filter').html().split("Search:").join("")).find('input').attr('placeholder', 'Search...');
+    $('.panel-body #attendance_filter').addClass('hide');
+    $('#attendance_filter input').keyup(function(){
+      attendanceTable.search($(this).val()).draw();
+    })
+}
+
+function chartsFilter(j, t, s, cAttendance, cDailyOt){
+  $('#summary-panel #mar').html(j[t][s]['report']['attrate']);
+  $('#summary-panel #hr').html(j[t][s]['report']['totalhr']);
+  $('#summary-panel #ot').html(j[t][s]['report']['totalothr']);
+  $('#summary-panel #otn').html(j[t][s]['report']['wdot']);
+  $('#summary-panel #phot').html(j[t][s]['report']['weot']);
+  $('#summary-panel #high').html($('#inject-employee .orange').length);
+  $('#summary-panel #low').html($('#inject-employee .yellow').length);
+  $('#summary-panel #miss').html($('#inject-employee .marron').length);
+  $('#summary-panel #leaves').html($('#inject-employee .blue').length);
+
+  cAttendance.series[0].remove();
+  cAttendance.series[0].remove();
+  cDailyOt.series[0].remove();
+
+  cAttendance.addSeries({
+        name: 'Attendance - ' + $('#filters .teams').find('.label-primary').text(),
+        data: j[t][s]['attrate']['data'],
+    });
+  cAttendance.addSeries({
+        name: 'Overtime - ' + $('#filters .teams').find('.label-primary').text(),
+        data: j[t][s]['otconso']['data'],
+        yAxis: 1    
+    });
+  cDailyOt.addSeries({
+    name : $('#filters .teams').find('.label-primary').text(),
+    data: j[t][s]['dailyot']['data']
+  });
+
+}
+
+function displayDetails($this){
+    $.get(
+        ajaxAttDetails,               
+    {
+      y: $('.yearpick').val(),
+      m: parseInt($('.monthpick').val()) + 1,
+      d: $this.find('div:first').attr('data-d'),
+      e: $this.find('div:first').attr('data-e')
+    }, 
+    function(r){
+      $('#e-name').html(r.det['name']);
+      $('#e-date').html(r.det['date']);
+      $('#e-tot').html(r.det['tothr']);
+      $('#e-mh').html(r.det['totreg']);
+      $('#e-ot').html(r.det['totot']);
+      $('#e-res').html(r.det['res']);
+
+      var $detTable = $('#input-det-table');
+      $('.det-content').find('.clone').remove();
+      $('.entry-panel .alert').addClass('hide');
+
+      if(r.det['tab']){
+          for (var i = 0; i < r.det['tab'].length; i++) {
+            $detTable.find('#inject-employee').children().remove();
+            $detTable.find('#input-det').html(r.det['tab'][i]['header']);
+            $detTable.find('th a').attr('href', r.det['tab'][i]['link'])
+            if( !r.det['tab'][i]['present']){$detTable.find('#inject-employee').html("<tr><td>Absent : "+r.det['tab'][i]['absence']+"</td></tr>");}
+            else if(r.det['tab'][i]['row']){
+                for (var j = 0; j < r.det['tab'][i]['row'].length; j++) {
+                    $detTable.find('#inject-employee').append(r.det['tab'][i]['row'][j]);
+                };
+            }
+            $('.det-content').append($detTable);
+            var $detTable = $detTable.clone().addClass('clone');
+          };
+      }
+
+      if ($this.hasClass('yellow')){
+        $('.entry-panel .alert-danger').removeClass('hide');
+        $('.entry-panel #error-msg').html("The hours recorded are too low!")
+      }else if ($this.hasClass('orange')){
+        $('.entry-panel .alert-danger').removeClass('hide');
+        $('.entry-panel #error-msg').html("The hours recorded are too high!")
+      }else{
+        $('.entry-panel .alert-success').removeClass('hide');
+      }
+
+      $("*[data-toggle='tooltip']").tooltip({html :'true',container: 'body'});
+      
+    },
+    "json");
+}
