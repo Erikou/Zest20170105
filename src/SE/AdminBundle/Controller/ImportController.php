87d4b77e748fc8b2b76e80a8c1b737d69469ab86
Louis Aumaitre <darksteelpenguin@gmail.com>
2016-09-30 11:56:09 +0800

    Fix SAP import, add deletion

--- src/SE/AdminBundle/Controller/ImportController.php
+++ src/SE/AdminBundle/Controller/ImportController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ImportController extends Controller
 {
+	private $info;
+	
 	public function sapnwrfcAction()
 	{
     	$connection = new SapConnection();
@@ -53,7 +55,8 @@ class ImportController extends Controller
         $em->flush();
 
         return $this->render('SEAdminBundle:Import:sap_import.html.twig', array(
-            'listSapImport' => $listSapImport
+            'listSapImport' => $listSapImport,
+        	'info' => $this->info
             ));
     }
     
@@ -75,6 +78,8 @@ class ImportController extends Controller
     	$sapCo->dataPersist($em, $date);
     	$sapCo->fileClose();
     	
+    	$this->info = "";
+    	
     	return $this->redirectToRoute('se_admin_import_sap');
     }
     
@@ -93,6 +98,59 @@ class ImportController extends Controller
     	$sapCo->dataPersist($em, $date);
     	$sapCo->fileClose();
     	
+    	$this->info = "";
+    	
+    	return $this->redirectToRoute('se_admin_import_sap');
+    }
+    
+    public function deleteAction($import_id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$idImport = $import_id;
+    
+    	$rowsNB=0;
+    	$importsNB=0;
+    	$inputsNB=0;
+    	$entriesNB=0;
+    
+    	$deleteImport = $em->getRepository('SEInputBundle:SapImports')->findOneBy(array('id' => $idImport));
+    	if($deleteImport){
+    		$dateImport = $deleteImport->getDate();
+    
+    		// if duplicate import, take all of them
+    		$duplicateImport = $em->getRepository('SEInputBundle:SapImports')->findBy(array('date' => $dateImport));
+    		$allSAProws = $em->getRepository('SEInputBundle:SAPRF')->getDayLines($dateImport);
+    
+    		//delete all that shit
+    		foreach ($allSAProws as $r) {
+    			$rowsNB+=1;
+    			$em->remove($r);
+    		}
+    		foreach ($duplicateImport as $d) {
+    			$importsNB+=1;
+    			$em->remove($d);
+    		}
+    		$resetInputs = $em->getRepository('SEInputBundle:UserInput')->findBy(array('dateInput' => $dateImport));
+    		foreach ($resetInputs as $i) {
+    			$i->setTotalToInput(0);
+    			$i->setManualTo(0);
+    			$i->setAutoTo(0);
+    			$i->setProcess(0);
+    			$inputsNB+=1;
+    			foreach ($i->getInputEntries() as $e) {
+    				$e->setTotalTo(0);
+    				$entriesNB+=1;
+    			}
+    		}
+    
+    		$em->flush();
+    
+    		$this->info = array("code" => 100, "success" => true, "comment" => "Import(s) deleted: ".$importsNB." - SAP Lines deleted: ".$rowsNB." - Inputs resetted: ".$inputsNB." - Entries resetted: ".$entriesNB);
+    
+    	}else{
+    		$this->info = array("code" => 400, "success" => false, "comment" => "Import not found");
+    	}
+
     	return $this->redirectToRoute('se_admin_import_sap');
     }
 }
--- src/SE/InputBundle/SAP/sapConnection.php
+++ src/SE/InputBundle/SAP/sapConnection.php
@@ -31,24 +31,50 @@ class sapConnection
 
     public function readTable()
     {
+    	ini_set('memory_limit', '1000M'); //512M
+    	ini_set('max_execution_time', 600); //10min
+    	ini_set('display_errors', 1);
+    	error_reporting(E_ALL);
+    	
         $full_file = fread($this->file, filesize($this->fileName));
-        $rows = explode("| TO Number|Material |Conf.dt. |Conf.t. |User |Typ|Source Bin|Typ|Dest. Bin |SLoc|WhN|Conf.dt. |ConfTme |Item|MvT|Created |Time |User |C| TR Number|Group |Actual qty|AUn|Delivery | Item|", $full_file);
-        foreach ($rows as $row => $row_data) {
-            if ($row == 0) continue;                // first header, no data here
-            $split_data = explode("|", $row_data);  // Split information
-            $col = 26;                              // Total number of columns
-            $nb = (sizeof($split_data) - 3) / $col; // Number of elements
-            for ($i = 0; $i < $nb; $i++) {
-                $this->results[$row-1][$i]['transfer_order'] = $split_data[$i * $col + 3];
-                $this->results[$row-1][$i]['material'] = $split_data[$i * $col + 4];
-                $this->results[$row-1][$i]['date_confirmation'] = $split_data[$i * $col + 5];
-                $this->results[$row-1][$i]['time_confirmation'] = $split_data[$i * $col + 6];
-                $this->results[$row-1][$i]['user'] = $split_data[$i * $col + 7];
-                $this->results[$row-1][$i]['source_storage_type'] = $split_data[$i * $col + 8];
-                $this->results[$row-1][$i]['source_storage_bin'] = $split_data[$i * $col + 9];
-                $this->results[$row-1][$i]['destination_storage_type'] = $split_data[$i * $col + 10];
-                $this->results[$row-1][$i]['destination_storage_bin'] = $split_data[$i * $col + 11];
-                $this->results[$row-1][$i]['storage_location'] = $split_data[$i * $col + 12];
+        if ($full_file[0] == "|"){
+        	$split_data = explode("|", $full_file);  // Split information
+        	$col = 26;                              // Total number of columns
+        	$nb = sizeof($split_data) / $col; // Number of elements
+        	$this->results = array();
+        	$this->results[0] = array();
+        	for ($i = 0; $i < $nb-1; $i++) {
+        		$this->results[0][$i] = array();
+        		$this->results[0][$i]['transfer_order'] = $split_data[$i * $col + 1];
+        		$this->results[0][$i]['material'] = $split_data[$i * $col + 2];
+        		$this->results[0][$i]['date_confirmation'] = $split_data[$i * $col + 3];
+        		$this->results[0][$i]['time_confirmation'] = $split_data[$i * $col + 4];
+        		$this->results[0][$i]['user'] = $split_data[$i * $col + 5];
+        		$this->results[0][$i]['source_storage_type'] = $split_data[$i * $col + 6];
+        		$this->results[0][$i]['source_storage_bin'] = $split_data[$i * $col + 7];
+        		$this->results[0][$i]['destination_storage_type'] = $split_data[$i * $col + 8];
+        		$this->results[0][$i]['destination_storage_bin'] = $split_data[$i * $col + 9];
+        		$this->results[0][$i]['storage_location'] = $split_data[$i * $col + 10];
+        	}
+        } else {
+        	$rows = explode("| TO Number|Material |Conf.dt. |Conf.t. |User |Typ|Source Bin|Typ|Dest. Bin |SLoc|WhN|Conf.dt. |ConfTme |Item|MvT|Created |Time |User |C| TR Number|Group |Actual qty|AUn|Delivery | Item|", $full_file);
+        	foreach ($rows as $row => $row_data) {
+            	if ($row == 0) continue;                // first header, no data here
+            	$split_data = explode("|", $row_data);  // Split information
+            	$col = 26;                              // Total number of columns
+            	$nb = (sizeof($split_data) - 3) / $col; // Number of elements
+            	for ($i = 0; $i < $nb; $i++) {
+                	$this->results[$row-1][$i]['transfer_order'] = $split_data[$i * $col + 3];
+                	$this->results[$row-1][$i]['material'] = $split_data[$i * $col + 4];
+                	$this->results[$row-1][$i]['date_confirmation'] = $split_data[$i * $col + 5];
+               	 	$this->results[$row-1][$i]['time_confirmation'] = $split_data[$i * $col + 6];
+                	$this->results[$row-1][$i]['user'] = $split_data[$i * $col + 7];
+                	$this->results[$row-1][$i]['source_storage_type'] = $split_data[$i * $col + 8];
+                	$this->results[$row-1][$i]['source_storage_bin'] = $split_data[$i * $col + 9];
+                	$this->results[$row-1][$i]['destination_storage_type'] = $split_data[$i * $col + 10];
+                	$this->results[$row-1][$i]['destination_storage_bin'] = $split_data[$i * $col + 11];
+                	$this->results[$row-1][$i]['storage_location'] = $split_data[$i * $col + 12];
+            	}
             }
         }
         // Debug print
@@ -73,6 +99,11 @@ class sapConnection
     }
 
     public function dataPersist($em, $date){
+    	ini_set('memory_limit', '1000M'); //512M
+    	ini_set('max_execution_time', 600); //10min
+    	ini_set('display_errors', 1);
+    	error_reporting(E_ALL);
+    	
     	$inputs = 0;
         for($i=0; $i<sizeof($this->results); $i++) {
             $data = $this->results[$i];
@@ -80,8 +111,8 @@ class sapConnection
                 $_saprf = new SAPRF;
                 $_saprf->setTransferOrder($data[$j]['transfer_order']);
                 $_saprf->setMaterial($data[$j]['material']);
-                $_saprf->setDateConfirmation(\DateTime::createFromFormat('d.m.Y', $data[$j]['date_confirmation']));
-                $_saprf->setTimeConfirmation(\DateTime::createFromFormat('H:i:s', $data[$j]['time_confirmation']));
+                //$_saprf->setDateConfirmation(\DateTime::createFromFormat('d.m.Y', $data[$j]['date_confirmation']));
+                //$_saprf->setTimeConfirmation(\DateTime::createFromFormat('H:i:s', $data[$j]['time_confirmation']));
                 $_saprf->setUser($data[$j]['user']);
                 $_saprf->setSourceStorageType($data[$j]['source_storage_type']);
                 $_saprf->setSourceStorageBin($data[$j]['source_storage_bin']);
@@ -89,7 +120,7 @@ class sapConnection
                 $_saprf->setDestinationStorageBin($data[$j]['destination_storage_bin']);
                 $_saprf->setDateImport($date);
                 $_saprf->setStorageLocation($data[$j]['storage_location']);
-
+                
                 $em->persist($_saprf);
                 $inputs++;
             }
